c1dd93d79122cd0bae94f93ab8aad97a42d567d5
a8306102002 <a8306102002@yahoo.com.tw>
2015-05-05 21:41:00 +0800

    Create 1234.js

---
+++ 1234.js
@@ -0,0 +1,49 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+    background-color: #00FFCC;
+}
+
+h1 {
+    color: blue;
+    text-align: center;
+}
+
+p {
+    font-family: "Times New Roman";
+    font-size: 20px;
+}
+</style>
+<meta charset='utf-8'>
+</head>
+<body>
+<h1>Welcome</h1>
+<p>This is my webside</p>
+
+<img src="http://i.forbesimg.com/media/lists/companies/google_416x416.jpg"/><BR/>
+<a href="http://tw.yahoo.com/">Yahoo!</a>
+<a href="https://www.google.com.tw/">Google</a><BR/><BR/><BR/>
+Introdution
+<table border="1">
+<tbody>
+<tr><td>學校</td><td>金門大學</td></tr>
+<tr><td>學系</td><td>資訊工程學系</td></tr>
+<tr><td>學年</td><td>學年三</td></tr>
+</tbody>
+</table><BR/>
+
+<strong>Take a break</strong><BR/>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/D9ksLn6hZ7Q" frameborder="0" allowfullscreen></iframe><BR/>
+<ul class="navbar">
+  <li><a href="123.html">參考資料</a>
+</ul>
+
+
+
+
+</body>
+</html>
+
